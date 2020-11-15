27777acf2b57fa54b616e5d8cca8260ce8320e85
Khaled Hasan <khaled_chobi@yahoo.com>
2020-11-14 23:32:19 -0500

    New Branch

--- src/odyssey/java/assignments/decision_structure/Decision_Structure.java
+++ src/odyssey/java/assignments/decision_structure/Decision_Structure.java
@@ -26,4 +26,3 @@ public class Decision_Structure {
     }
 
 }
-// Tested
---
+++ src/odyssey/java/assignments/decision_structure/Test.java
@@ -0,0 +1,9 @@
+package src.odyssey.java.assignments.decision_structure;
+
+public class Test {
+    public static void main(String args[]){
+
+            System.out.println("Assignment followed by Naim bhai and Shamim bhai");
+
+    }
+}
